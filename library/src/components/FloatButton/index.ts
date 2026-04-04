f2c1a1fcc25b56fb374f3689f28a97c6541183a7
AzureHeart <249638467@qq.com>
2026-04-04 18:54:56 +0800

    feat: 添加浮动按钮组件
    新增一个浮动按钮组件，支持拖拽和双击事件，并且可以根据边界容器自动调整位置。
    
    - 实现了拖拽功能，通过 `useDraggable` 和 `useElementBounding` 等VueUse工具函数实现了对组件容器的拖拽操作，并且在拖拽过程中实时调整按钮的位置以确保不超出安全边界。
    - 添加了双击事件支持，使用 `vDoubleTap` 自定义指令实现了对主按钮的双击事件监听。
    - 组件样式支持亮色和暗色模式，并且在移动端环境下修复了拓展失效的问题。
    - 提供了 `initPosition` 和 `safeBoundary` 属性，分别用于设置按钮的初始位置和安全边界，防止按钮超出边界容器。
    - 实现了按钮的展开和收起功能，当鼠标悬停或点击主按钮时，额外的按钮会以动画形式展开。

---
+++ library/src/components/FloatButton/index.ts
@@ -0,0 +1,5 @@
+export { default as BaseFloatButton } from "./FloatButton.vue";
+export type {
+  FloatButtonProps as BaseFloatButtonProps,
+  FloatButtonEmits as BaseFloatButtonEmits,
+} from "./types";
---
+++ library/src/components/FloatButton/types.ts
@@ -0,0 +1,28 @@
+import type { TapEvent } from "@/hooks/useBaseTap";
+import type { MaybeComputedElementRef } from "@vueuse/core";
+
+export interface FloatButtonProps {
+  /** 边界容器 (如果不传入则默认使用窗口尺寸)  */
+  boundaryContainer?: MaybeComputedElementRef;
+  /** 初始位置 (相对于右下角定位)
+   * @default
+   * { right: 10, bottom: 20 }  */
+  initPosition?: {
+    right: number;
+    bottom: number;
+  };
+  /** 安全边界
+   * @default
+   * { left: 4, top: 4, right: 4, bottom: 4 } */
+  safeBoundary?: {
+    left: number;
+    top: number;
+    right: number;
+    bottom: number;
+  };
+}
+
+export interface FloatButtonEmits {
+  "main-button-click": [e?: MouseEvent];
+  "main-button-dblclick": [e?: TapEvent];
+}
